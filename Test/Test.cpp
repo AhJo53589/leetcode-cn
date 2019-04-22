43fd92a4b354fd0351bc0158c84e1a6976d195f1
Ikaruga <ahjo_bb@hotmail.com>
2019-04-22 17:01:46 +0800

    26.RemoveDuplicates

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -4,18 +4,52 @@
 #include "pch.h"
 #include <iostream>
 
-int main()
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums)	// 32ms
 {
-    std::cout << "Hello World!\n"; 
+	if (nums.size() == 0) return 0;
+	if (nums.size() == 1) return 1;
+
+	int iNewLen = 0;
+	for (int i = 1; i < nums.size(); i++)
+	{
+		if (nums[i] != nums[iNewLen])
+		{
+			++iNewLen;
+			if (iNewLen != i)
+			{
+				nums[iNewLen] = nums[i];
+			}
+		}
+	}
+	return iNewLen + 1;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+//int removeDuplicates(vector<int>& nums)		// 20ms
+//{
+//	if (nums.size() == 0) return 0;
+//	auto i = 0;
+//	auto j = 0;
+//	while (j < nums.size()) 
+//	{
+//		if (nums[i] == nums[j])
+//			j++;
+//		else
+//			nums[++i] = nums[j];
+//	}
+//	return i + 1;
+//}
+//
+int main()
+{
+	int A[] = { 0,0,0,1,1,2,3,3,4,5 };
+	vector<int> nums(&A[0], &A[10]);
+	int len = removeDuplicates(nums);
+	for (int i = 0; i < len; i++)
+	{
+		cout << nums[i] << " ";
+	}
+	cout << endl << len << endl;
+}
