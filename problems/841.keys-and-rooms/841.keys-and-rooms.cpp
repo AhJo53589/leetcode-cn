82fcb670b3182aa3cd425f378c0a50a6b097fbd2
Ikaruga <ahjo_bb@hotmail.com>
2019-06-26 15:47:08 +0800

    542.01-matrix & 733.flood-fill & 841.keys-and-rooms

--- problems/841.keys-and-rooms/841.keys-and-rooms.cpp
+++ problems/841.keys-and-rooms/841.keys-and-rooms.cpp
@@ -4,18 +4,54 @@
 #include "pch.h"
 #include <iostream>
 
-int main()
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <random>
+
+#include "..\Common\Common.h"
+#include "..\Common\GraphNode.h"
+#include "..\Common\TreeNode.h"
+using namespace std;
+
+void visitRoom(vector<vector<int>>& rooms, int roomId, unordered_set<int>& visited)
 {
-    std::cout << "Hello World!\n"; 
+	if (roomId >= rooms.size()) return;
+
+	visited.insert(roomId);
+	for (auto r : rooms[roomId])
+	{
+		if (!visited.count(r))
+		{
+			visitRoom(rooms, r, visited);
+		}
+	}
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+bool canVisitAllRooms(vector<vector<int>>& rooms)
+{
+	unordered_set<int> visited;
+	visitRoom(rooms, 0, visited);
+	for (int i = 0; i < rooms.size(); i++)
+	{
+		if (!visited.count(i)) return false;
+	}
+	return true;
+}
 
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+int main()
+{
+	//string strInit = "[[1],[2],[3],[]]";
+	string strInit = "[[1,3],[3,0,1],[2],[0]]";
+
+	vector<vector<int>> m = StringToVectorVectorInt(strInit);
+
+	cout << canVisitAllRooms(m) << endl;
+
+}
