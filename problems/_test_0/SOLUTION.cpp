6f0c6a61819f6eccad1b6a694246ef5f3c163e05
Ikaruga <ahjo_bb@hotmail.com>
2019-12-20 15:50:29 +0800

    329.longest-increasing-path-in-a-matrix

--- problems/_test_0/SOLUTION.cpp
+++ problems/_test_0/SOLUTION.cpp
@@ -1,62 +1,74 @@
 
-//////////////////////////////////////////////////////////////////////////
-void calc(vector<vector<int>>& m, int x, int y, vector<vector<int>>& c, vector<vector<int>>& dd)
-{
-    if (x < 0 || x >= m.size() || y < 0 || y >= m[0].size()) return;
 
-    int cnt = -1;
-    for (auto i = 0; i < dd.size(); i++)
-    {
-        int dx = x + dd[i][0];
-        int dy = y + dd[i][1];
-        if (dx < 0 || dx >= m.size() || dy < 0 || dy >= m[0].size())
-        {
-            continue;
-        }
-        if (m[dx][dy] < m[x][y])
-        {
-            if (c[dx][dy] == -1) return;
-            cnt = max(cnt, c[dx][dy]);
-        }
-    }
-    c[x][y] = cnt + 1;
-}
-
-int longestIncreasingPath(vector<vector<int>>& matrix) 
-{
-    if (matrix.size() == 0) return 0;
-    vector<vector<int>> dd = {{0,1},{0,-1},{1,0},{-1,0}};
-    vector<vector<int>> c(matrix.size(), vector<int>(matrix[0].size(), -1));
-
-    int ans = 0;
-    bool flag = false;
-    while (!flag)
-    {
-        flag = true;
-        for (int x = 0; x < matrix.size(); x++)
-        {
-            for (int y = 0; y < matrix[0].size(); y++)
-            {
-                if (c[x][y] < 0)
-                {
-                    calc(matrix, x, y, c, dd);
-                }
-                if (c[x][y] < 0)
-                {
-                    flag = false;
-                }
-                ans = max(ans, c[x][y]);
-            }
-        }
-    }
-    return ans + 1;
-}
 
 //////////////////////////////////////////////////////////////////////////
-int _solution_run(vector<vector<int>>& matrix)
+int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
 {
-    printVectorVectorT(matrix);
-	return longestIncreasingPath(matrix);
+	if (beginWord == endWord) return 1;
+	bool flag = true;
+
+	map<string, vector<string>> comboList;
+	for (auto w : wordList)
+	{
+		if (w == endWord)
+		{
+			flag = false;
+		}
+		for (size_t i = 0; i < w.size(); i++)
+		{
+			auto temp = w;
+			temp[i] = '*';
+			comboList[temp].push_back(w);
+		}
+	}
+	if (flag) return 0;
+
+	unordered_map<string, int> vi[2];
+	queue<string> que[2];
+	que[0].push(beginWord);
+	vi[0][beginWord] = 1;
+	que[1].push(endWord);
+	vi[1][endWord] = 1;
+
+	while (!que[0].empty() || !que[1].empty())
+	{
+		int k = (que[0].size() < que[1].size()) ? 0 : 1;
+		k = (que[k].size() == 0) ? (k + 1) % 2 : k;
+		auto& qr = (que[0].size() < que[1].size()) ? que[0] : que[1];
+		
+		for (int i = que[k].size(); i > 0; i--)
+		{
+			auto q = que[k].front();
+			que[k].pop();
+
+			for (size_t i = 0; i < q.size(); i++)
+			{
+				auto temp = q;
+				temp[i] = '*';
+
+				for (auto w : comboList[temp])
+				{
+					if (vi[k].count(w) != 0) continue;
+					int k2 = (k + 1) % 2;
+					if (vi[k2].count(w) != 0)
+					{
+						return vi[k2][w] + vi[k][q];
+					}
+
+					que[k].push(w);
+					vi[k][w] = vi[k][q] + 1;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////
+int _solution_run(string beginWord, string endWord, vector<string>& wordList)
+{
+	return ladderLength(beginWord,endWord,wordList);
 }
 
 //#define USE_SOLUTION_CUSTOM
