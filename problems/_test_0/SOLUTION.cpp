bc71d3f684a707ab4748be45a0e9c07f202720c5
Ikaruga <ahjo_bb@hotmail.com>
2019-12-27 18:02:25 +0800

    210.course-schedule-ii

--- problems/_test_0/SOLUTION.cpp
+++ problems/_test_0/SOLUTION.cpp
@@ -2,55 +2,102 @@
 
 
 //////////////////////////////////////////////////////////////////////////
-bool check(string& s, int beg, int end)
+class Trie
 {
-	if (beg > end) return false;
-	while (beg < end)
+public:
+	Trie() {}
+	void insert(string word)
 	{
-		if (s[beg++] != s[end--]) return false;
-	}
-	return true;
-}
-
-void partition(string& s, int beg, vector<string> part, set<vector<string>>& ans)
-{
-	for (int i = beg; i < s.size(); i++)
-	{
-		string sub;
-		for (int j = i; j < s.size(); j++)
+		Trie* node = this;
+		for (auto c : word)
 		{
-			sub += s[j];
-			if (!check(s, i, j)) continue;
-			part.push_back(sub);
-			partition(s, j + 1, part, ans);
-			if (j == s.size() - 1)
+			if (node->next[c - 'a'] == nullptr)
 			{
-				ans.insert(part);
+				node->next[c - 'a'] = new Trie();
 			}
-			part.pop_back();
+			node = node->next[c - 'a'];
 		}
-		part.push_back({ s[i] });
+		node->is_string = true;
 	}
+
+	bool search(string word)
+	{
+		Trie* node = this;
+		for (auto c : word)
+		{
+			if (node->next[c - 'a'] == nullptr) return false;
+			node = node->next[c - 'a'];
+		}
+		return node->is_string;
+	}
+
+	bool startsWith(string prefix)
+	{
+		Trie* node = this;
+		for (auto c : prefix)
+		{
+			if (node->next[c - 'a'] == nullptr) return false;
+			node = node->next[c - 'a'];
+		}
+		return true;
+	}
+
+private:
+	bool is_string = false;
+	Trie* next[26] = { nullptr };
+};
+
+void find(vector<vector<char>>& board, int x, int y, string fstr, set<vector<int>> &vi, set<string>& ret, Trie& trie, vector<vector<int>>& dd)
+{
+	if (vi.count({ x, y }) != 0) return;
+	if (x < 0 || x >= board.size()) return;
+	if (y < 0 || y >= board[0].size()) return;
+	fstr += board[x][y];
+	if (trie.search(fstr))
+	{
+		ret.insert(fstr);
+	}
+	if (!trie.startsWith(fstr)) return;
+
+	vi.insert({ x, y });
+	for (auto d : dd)
+	{
+		int dx = x + d[0];
+		int dy = y + d[1];
+		find(board, dx, dy, fstr, vi, ret, trie, dd);
+	}
+	vi.erase({ x, y });
 }
 
-vector<vector<string>> partition(string s) 
+vector<string> findWords(vector<vector<char>>& board, vector<string>& words)
 {
-	set<vector<string>> ans;
-	vector<string> part;
-	partition(s, 0, part, ans);
+	set<vector<int>> vi;
+	set<string> ret;
+	vector<vector<int>> dd = { {0, 1}, {0, -1}, {1,0},{-1,0} };
 
-	vector<vector<string>> ret(ans.begin(), ans.end());
-	return ret;
+	Trie trie;
+	for (auto w : words) trie.insert(w);
+
+	for (int x = 0; x < board.size(); x++)
+	{
+		for (int y = 0; y < board[0].size(); y++)
+		{
+			find(board, x, y, "", vi, ret, trie, dd);
+		}
+	}
+
+	vector<string> ans(ret.begin(), ret.end());
+	return ans;
 }
 
 //////////////////////////////////////////////////////////////////////////
-vector<vector<string>> _solution_run(string s)
+vector<string> _solution_run(vector<vector<char>>& board, vector<string>& words)
 {
-	return partition(s);
+	return findWords(board,words);
 }
 
 //#define USE_SOLUTION_CUSTOM
-//vector<vector<string>> _solution_custom(TestCases &tc)
+//vector<string> _solution_custom(TestCases &tc)
 //{
 //}
 
