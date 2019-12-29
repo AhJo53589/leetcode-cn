3c4783f92e414ef39c9195d0249ca8304af84435
unknown <ahjo_bb@hotmail.com>
2019-12-29 12:10:11 +0800

    update

--- problems/_test_0/SOLUTION.cpp
+++ problems/_test_0/SOLUTION.cpp
@@ -2,18 +2,74 @@
 
 
 //////////////////////////////////////////////////////////////////////////
-vector<int> pathsWithMaxScore(vector<string>& board) {
+int getNum(string word, unordered_map<char, int>& um)
+{
+	if (word.size() == 0 || um[word[0]] == 0) return -1;
+	int out = 0;
+	for (auto c : word)
+	{
+		out *= 10;
+		out += um[c];
+	}
+	return out;
+}
 
+bool calc(vector<string>& words, string result, unordered_map<char, int>& um)
+{
+	int left = 0;
+	for (auto w : words)
+	{
+		int t = getNum(w, um);
+		if (t == -1) return false;
+		left += t;
+	}
+	int right = getNum(result, um);
+	return left == right;
+}
+
+bool isSolvable(vector<string>& words, string result) 
+{
+	set<char> chars;
+	for (auto s : words)
+	{
+		for (auto c : s)
+		{
+			chars.insert(c);
+		}
+	}
+	for (auto c : result)
+	{
+		chars.insert(c);
+	}
+	vector<char> ch(chars.begin(), chars.end());
+
+	vector<int> nums;
+	for (int i = 0; i < 10; i++)
+	{
+		nums.push_back(i);
+	}
+
+	do 
+	{
+		unordered_map<char, int> um;
+		for (size_t i = 0; i < ch.size(); i++)
+		{
+			um[ch[i]] = nums[i];
+		}
+		if (calc(words, result, um)) return true;
+
+	} while (next_permutation(nums.begin(), nums.end()));
+	return false;
 }
 
 //////////////////////////////////////////////////////////////////////////
-vector<int> _solution_run(vector<string>& board)
+bool _solution_run(vector<string>& words, string result)
 {
-	return pathsWithMaxScore(board);
+	return isSolvable(words,result);
 }
 
 //#define USE_SOLUTION_CUSTOM
-//vector<int> _solution_custom(TestCases &tc)
+//bool _solution_custom(TestCases &tc)
 //{
 //}
 
