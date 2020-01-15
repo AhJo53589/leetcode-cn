ce5801353327e401190a87659aa78131fae9c1d3
Ikaruga <ahjo_bb@hotmail.com>
2020-01-15 18:14:47 +0800

    update string to map nodes

--- problems/longest-consecutive-sequence/README.md
+++ problems/longest-consecutive-sequence/README.md
@@ -2,23 +2,21 @@
 
 ### 题目描述
 <p>给定一个未排序的整数数组，找出最长连续序列的长度。</p>
-
 <p>要求算法的时间复杂度为&nbsp;<em>O(n)</em>。</p>
-
 <p><strong>示例:</strong></p>
-
 <pre><strong>输入:</strong>&nbsp;[100, 4, 200, 1, 3, 2]
 <strong>输出:</strong> 4
 <strong>解释:</strong> 最长连续序列是 <code>[1, 2, 3, 4]。它的长度为 4。</code></pre>
 
-
 ---
 ### 思路
 ```
+
 ```
 
 ### 答题
 ``` C++
+// 排序
 int longestConsecutive(vector<int>& nums) 
 {
 	if (nums.size() == 0) return 0;
@@ -39,5 +37,38 @@ int longestConsecutive(vector<int>& nums)
 
 ### 其它
 ``` C++
+// 哈希
+int longestConsecutive(vector<int>& nums) 
+{
+    if (nums.size() < 2)return nums.size();
+    int res = 0;
+    unordered_map<int, int> hashmap;
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        if (hashmap.count(nums[i]) == 0)
+        {
+            int left = 0, right = 0;
+            if (hashmap.count(nums[i] - 1) > 0)
+            {
+                left = hashmap[nums[i] - 1];
+            }
+            if (hashmap.count(nums[i] + 1) > 0)
+            {
+                right = hashmap[nums[i] + 1];
+            }
+            hashmap[nums[i]] = left + right + 1;
+            if (left != 0)
+            {
+                hashmap[nums[i] - left] = left + right + 1;
+            }
+            if (right != 0)
+            {
+                hashmap[nums[i] + right] = left + right + 1;
+            }
+            res = max(res, left + right + 1);
+        }
+    }
+    return res;
+}
 ```
 
--- problems/longest-consecutive-sequence/SOLUTION.cpp
+++ problems/longest-consecutive-sequence/SOLUTION.cpp
@@ -1,48 +1,115 @@
 
 //////////////////////////////////////////////////////////////////////////
-//int longestConsecutive(vector<int>& nums) {
-//	if (nums.empty()) {
-//		return 0;
-//	}
-//	unordered_set<int> myset(nums.begin(), nums.end());
-//	int res = 0;
-//
-//	for (auto &num : nums) {
-//		if (myset.count(num - 1) == 0) {
-//			int x = num + 1;
-//			while (myset.count(x)) {
-//				x++;
-//			}
-//			res = max(res, x - num);
-//		}
-//	}
-//	return res;
-//}
+// ¹þÏ£
+int longestConsecutive(vector<int>& nums) 
+{
+    if (nums.size() < 2)return nums.size();
+    int res = 0;
+    unordered_map<int, int> hashmap;
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        if (hashmap.count(nums[i]) == 0)
+        {
+            int left = 0, right = 0;
+            if (hashmap.count(nums[i] - 1) > 0)
+            {
+                left = hashmap[nums[i] - 1];
+            }
+            if (hashmap.count(nums[i] + 1) > 0)
+            {
+                right = hashmap[nums[i] + 1];
+            }
+            hashmap[nums[i]] = left + right + 1;
+            if (left != 0)
+            {
+                hashmap[nums[i] - left] = left + right + 1;
+            }
+            if (right != 0)
+            {
+                hashmap[nums[i] + right] = left + right + 1;
+            }
+            res = max(res, left + right + 1);
+        }
+    }
+    return res;
+}
+
 
 
 //////////////////////////////////////////////////////////////////////////
-int longestConsecutive(vector<int>& nums)
-{
-	if (nums.size() == 0) return 0;
-	sort(nums.begin(), nums.end());
+// ÅÅÐò
+//class Solution {
+//public:
+//    int longestConsecutive(vector<int>& nums)
+//    {
+//        if (nums.size() == 0) return 0;
+//        sort(nums.begin(), nums.end());
+//
+//        int ans = 1;
+//        int temp = 1;
+//        for (int i = 1; i < nums.size(); i++)
+//        {
+//            if (nums[i] == nums[i - 1]) continue;
+//            temp = (nums[i] == nums[i - 1] + 1) ? temp + 1 : 1;
+//            ans = max(ans, temp);
+//        }
+//
+//        return ans;
+//    }
+//};
 
-	int ans = 1;
-	int temp = 1;
-	for (int i = 1; i < nums.size(); i++)
-	{
-		if (nums[i] == nums[i - 1]) continue;
-		temp = (nums[i] == nums[i - 1] + 1) ? temp + 1 : 1;
-		ans = max(ans, temp);
-	}
 
-	return ans;
-}
 
+//////////////////////////////////////////////////////////////////////////
+// ²¢²é¼¯
+//class Solution {
+//public:
+//    int find(int x)
+//    {
+//        return (x == parnet[x]) ? x : (parnet[x] = find(parnet[x]));
+//    }
+//
+//    void unite(int x, int y)
+//    {
+//        if (parnet.count(x) == 0) return;
+//        if (parnet.count(y) == 0) return;
+//        int x0 = find(x);
+//        int y0 = find(y);
+//        if (x0 == y0) return;
+//        if (sz[x0] < sz[y0])
+//        {
+//            swap(x0, y0);
+//        }
+//        parnet[y0] = x0;
+//        sz[x0] += sz[y0];
+//    }
+//
+//    int longestConsecutive(vector<int>& nums)
+//    {
+//        int ans = 0;
+//        for (auto& n : nums)
+//        {
+//            if (parnet.count(n) != 0) continue;
+//            parnet[n] = n;
+//            sz[n] = 1;
+//
+//            unite(n, n - 1);
+//            unite(n, n + 1);
+//            ans = max(ans, sz[find(n)]);
+//        }
+//        return ans;
+//    }
+//
+//private:
+//    unordered_map<int, int> parnet;
+//    unordered_map<int, int> sz;
+//};
 
 //////////////////////////////////////////////////////////////////////////
 int _solution_run(vector<int>& nums)
 {
-	return longestConsecutive(nums);
+    Solution sln;
+	return sln.longestConsecutive(nums);
 }
 
 //#define USE_SOLUTION_CUSTOM
