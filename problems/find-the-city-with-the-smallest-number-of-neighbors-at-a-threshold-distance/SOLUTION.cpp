73959513260a2f8409864b9d4bee03bdb9987899
AhJo53589 <ahjo_bb@hotmail.com>
2020-01-26 17:00:16 +0800

    5322.minimum-difficulty-of-a-job-schedule

--- problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/SOLUTION.cpp
+++ problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/SOLUTION.cpp
@@ -2,41 +2,45 @@
 
 
 //////////////////////////////////////////////////////////////////////////
-void dfs(vector<vector<int>>& city_dis, int cur, int threshold, unordered_map<int, int>& vi)
-{
-	for (int i = 0; i < city_dis[cur].size(); i++)
-	{
-		if (city_dis[cur][i] == -1) continue;
-		if (vi.count(i) != 0 && vi[i] <= threshold) continue;
-		if (threshold < city_dis[cur][i]) continue;
-		vi[i] = threshold - city_dis[cur][i];
-		dfs(city_dis, i, threshold - city_dis[cur][i], vi);
-	}
-}
-
 int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold)
 {
-	vector<vector<int>> city_dis(n, vector<int>(n, -1));
+	vector<vector<int>> dis(n, vector<int>(n, INT_MAX));
 	for (auto& edge : edges)
 	{
-		city_dis[edge[0]][edge[1]] = edge[2];
-		city_dis[edge[1]][edge[0]] = edge[2];
+		int u = edge[0];
+		int v = edge[1];
+		dis[u][v] = min(edge[2], dis[u][v]);
+		dis[v][u] = min(edge[2], dis[v][u]);
 	}
-
-	int ans = -1;
-	int cnt = n;
-	vector<vector<int>> citys(n, vector<int>());
 	for (int i = 0; i < n; i++)
 	{
-		unordered_map<int, int> vi;
-		vi[i] = distanceThreshold;
-		dfs(city_dis, i, distanceThreshold, vi);
-		if (cnt >= vi.size() - 1)
+		for (int u = 0; u < n; u++)
 		{
-			cnt = vi.size() - 1;
+			if (i == u) continue;
+			for (int v = 0; v < n; v++)
+			{
+				if (v == u || v == i) continue;
+				if (dis[u][i] == INT_MAX || dis[i][v] == INT_MAX) continue;
+				dis[u][v] = min(dis[u][v], dis[u][i] + dis[i][v]);
+				dis[v][u] = min(dis[u][v], dis[v][u]);
+			}
+		}
+	}
+
+	int minn = INT_MAX;
+	int ans = -1;
+	for (int i = n - 1; i >= 0; i--)
+	{
+		int cnt = 0;
+		for (int nex = 0; nex < n; nex++)
+		{
+			cnt += (i != nex && dis[i][nex] <= distanceThreshold);
+		}
+		if (cnt < minn)
+		{
+			minn = cnt;
 			ans = i;
 		}
-		cout << i << ": [vi] = " << vi.size() - 1 << "\t[cnt] = " << cnt << endl;
 	}
 	return ans;
 }
--- problems/minimum-difficulty-of-a-job-schedule/SOLUTION.cpp
+++ problems/minimum-difficulty-of-a-job-schedule/SOLUTION.cpp
@@ -2,8 +2,46 @@
 
 
 //////////////////////////////////////////////////////////////////////////
-int minDifficulty(vector<int>& jobDifficulty, int d) {
+void dfs(vector<vector<int>>& saving, int cur, int overJobs, int saved, int& maxSaved)
+{
+	if (overJobs == 0)
+	{
+		maxSaved = max(saved, maxSaved);
+		return;
+	}
+	for (size_t j = 0; j < overJobs + 1; j++)
+	{
+		int k = cur + j;
+		if (k >= saving[j].size()) break;
+		dfs(saving, k + 1, overJobs - j, saved + saving[j][cur], maxSaved);
+	}
+}
 
+int minDifficulty(vector<int>& jobDifficulty, int d) 
+{
+	int n = jobDifficulty.size();
+	if (d == 1) return *max_element(jobDifficulty.begin(), jobDifficulty.end());
+	int ans = accumulate(jobDifficulty.begin(), jobDifficulty.end(), 0);
+	int overJobs = jobDifficulty.size() - d;
+	if (overJobs < 0) return -1;
+	if (overJobs == 0) return ans;
+
+	vector<vector<int>> jobs(jobDifficulty.size() + 1, vector<int>(d + 1, 0));
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= min(d, i); j++)
+		{
+			jobs[i][j] = INT_MAX;
+			int mx = 0;
+			for (int k = i - 1; k >= j - 1; k--)
+			{
+				if (jobs[k][j - 1] == INT_MAX) continue;
+				mx = max(mx, jobDifficulty[k]);
+				jobs[i][j] = min(jobs[i][j], jobs[k][j - 1] + mx);
+			}
+		}
+	}
+	return jobs[n][d];
 }
 
 //////////////////////////////////////////////////////////////////////////
