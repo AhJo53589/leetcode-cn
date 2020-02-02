c71a5e072cd9f9a688f8181babeb2662144d29c9
AhJo53589 <ahjo_bb@hotmail.com>
2020-02-02 13:25:57 +0800

    5330.maximum-product-of-splitted-binary-tree

--- problems/jump-game-v/SOLUTION.cpp
+++ problems/jump-game-v/SOLUTION.cpp
@@ -1,5 +1,34 @@
 
-
+//////////////////////////////////////////////////////////////////////////
+//int maxJumps(vector<int>& arr, int d) 
+//{
+//	int n = arr.size();
+//
+//	vector<int> p(n);
+//	for (int i = 0; i < n; ++i) p[i] = i;
+//	sort(p.begin(), p.end(), [&](int i, int j)
+//		{
+//			return arr[i] < arr[j];
+//		});
+//
+//	vector<int> f(n);
+//	for (int k = 0; k < n; ++k)
+//	{
+//		int i = p[k];
+//		int L = i - 1, R = i + 1;
+//		while (L >= 0 && arr[L] < arr[i]) --L;
+//		while (R < n && arr[R] < arr[i]) ++R;
+//		f[i] = 1;
+//		for (int j = L + 1; j <= R - 1; ++j)
+//		{
+//			if (j != i && abs(j - i) <= d)
+//			{
+//				f[i] = max(f[i], f[j] + 1);
+//			}
+//		}
+//	}
+//	return *max_element(f.begin(), f.end());
+//}
 
 //////////////////////////////////////////////////////////////////////////
 void maxJumps(vector<int>& arr, int d, int cur, vector<int>& steps, int& ans)
