a8877376365fd74d9a41a58b049f6f5539b0b07e
unknown <ahjo_bb@hotmail.com>
2020-02-09 18:28:20 +0800

    5335.maximum-students-taking-exam

--- problems/maximum-students-taking-exam/SOLUTION.cpp
+++ problems/maximum-students-taking-exam/SOLUTION.cpp
@@ -7,14 +7,14 @@ public:
 		vector<vector<int>> dd = { {0, -1}, {0, 1}, {-1, -1}, {-1, 1} };
 		int M = seats.size();
 		int N = seats[0].size();
-		vector<vector<int>> valid;
-		vector<vector<vector<vector<int>>>> op(M, vector<vector<vector<int>>>(N, vector<vector<int>>()));
+		vector<int> valid;
+		vector<vector<int>> op(M * N, vector<int>());
 		for (int i = 0; i < M; i++)
 		{
 			for (int j = 0; j < N; j++)
 			{
 				if (seats[i][j] == '#') continue;
-				valid.push_back({ i, j });
+				valid.push_back(i * N + j);
 				for (auto d : dd)
 				{
 					int dx = i + d[0];
@@ -22,7 +22,8 @@ public:
 					if (dx < 0 || dx >= M) continue;
 					if (dy < 0 || dy >= N) continue;
 					if (seats[dx][dy] == '#') continue;
-					op[i][j].push_back({ dx, dy });
+					op[i * N + j].push_back(dx * N + dy);
+					op[dx * N + dy].push_back(i * N + j);
 				}
 			}
 		}
@@ -31,7 +32,7 @@ public:
 		for (int cur = 0; cur < (1 << valid.size()); cur++)
 		{
 			int cnt = 0;
-			set <vector<int>> vi;
+			set<int> vi;
 			for (int i = 0; i < valid.size(); i++)
 			{
 				if (((1 << i) & cur) == 0) continue;
@@ -40,7 +41,7 @@ public:
 					cnt = 0;
 					break;
 				}
-				for (auto &p : op[valid[i][0]][valid[i][1]])
+				for (auto &p : op[valid[i]])
 				{
 					vi.insert(p);
 				}
@@ -55,9 +56,9 @@ public:
 //////////////////////////////////////////////////////////////////////////
 int _solution_run(vector<vector<char>>& seats)
 {
-	int caseNo = 3;
-	static int caseCnt = 0;
-	if (caseNo != -1 && caseCnt++ != caseNo) return {};
+	//int caseNo = 3;
+	//static int caseCnt = 0;
+	//if (caseNo != -1 && caseCnt++ != caseNo) return {};
 
 	printVectorVectorT(seats);
 	Solution sln;
