ae62f74d1738c1f8dabe4b116aebbc2a64f6f180
AhJo53589 <ahjo_bb@hotmail.com>
2020-02-14 17:30:30 +0800

    update leetcode_cpp_helper.exe

--- git_commit.bat
+++ git_commit.bat
@@ -1,4 +1,4 @@
 git pull
 git add -A
-git commit -m"703"
+git commit -m"update leetcode_cpp_helper.exe"
 git push
--- problems_test/0/SOLUTION.cpp
+++ problems_test/0/SOLUTION.cpp
@@ -1,44 +1,21 @@
 
 //////////////////////////////////////////////////////////////////////////
-class Solution {
-public:
-	vector<vector<string>> test(vector<vector<string>>& a)
-	//vector<vector<char>> test(vector<vector<char>>& a)
-	//int test(vector<string>& a)
-	{
-		return a;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
     }
-};
 
 //////////////////////////////////////////////////////////////////////////
-vector<vector<string>> _solution_run(vector<vector<string>>& a)
-//vector<vector<char>> _solution_run(vector<vector<char>>& a)
-//int _solution_run(vector<string>& a)
+vector<int> _solution_run(vector<int>& nums, int target)
 {
-	//int caseNo = 5;
+	//int caseNo = -1;
 	//static int caseCnt = 0;
 	//if (caseNo != -1 && caseCnt++ != caseNo) return {};
-	
-	printVectorVectorT(a);
-	//for (auto& vt : a)
-	//{
-	//	for (auto& t : vt)
-	//	{
-	//		cout << t << endl;
-	//	}
-	//}
-	//		
-	//for (auto& t : a)
-	//{
-	//	cout << t << endl;
-	//}
 
-	Solution sln;
-	return sln.test(a);
+	return twoSum(nums, target);
 }
 
 //#define USE_SOLUTION_CUSTOM
-//int _solution_custom(TestCases &tc)
+//string _solution_custom(TestCases &tc)
 //{
 //	return {};
 //}
