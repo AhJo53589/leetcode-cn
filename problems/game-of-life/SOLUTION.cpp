120426674375897f1ef316b0c3f93f43a3ac727f
AhJo53589 <ahjo_bb@hotmail.com>
2020-04-02 12:30:46 +0800

    137.single-number-ii

--- problems/game-of-life/SOLUTION.cpp
+++ problems/game-of-life/SOLUTION.cpp
@@ -2,63 +2,101 @@
 
 
 //////////////////////////////////////////////////////////////////////////
-void gameOfLife(vector<vector<int>>& board)
-{
-	size_t m = board.size();
-	if (m == 0) return;
-	size_t n = board[0].size();
+class Solution2 {
+public:
+    void aliveReport(vector<vector<int>>& board, int x, int y)
+    {
+        for (int i = max(0, x - 1); i < min((int)board.size(), x + 2); i++)
+        {
+            for (int j = max(0, y - 1); j < min((int)board[0].size(), y + 2); j++)
+            {
+                if (i == x && j == y) continue;
+                board[i][j] += (board[i][j] > 0) ? 1 : -1;
+            }
+        }
+    }
 
-	auto getNear = [m, n](size_t _i, size_t _j, vector<vector<size_t>> &pos)
-	{
-		for (size_t i = max((size_t)1, _i) - 1; i < min(m, _i + 2); i++)
-		{
-			for (size_t j = max((size_t)1, _j) - 1; j < min(n, _j + 2); j++)
-			{
-				if (i == _i && j == _j) continue;
-				pos.push_back({ i, j });
-			}
-		}
-	};
+    void gameOfLife(vector<vector<int>>& board)
+    {
+        if (board.empty() || board[0].empty()) return;
 
-	for (size_t i = 0; i < board.size(); i++)
-	{
-		for (size_t j = 0; j < board[i].size(); j++)
-		{
-			if (board[i][j] > 0)
-			{
-				vector<vector<size_t>> pos;
-				getNear(i, j, pos);
-				for (auto &p : pos)
-				{
-					int &x = board[p[0]][p[1]];
-					x = (x > 0) ? x + 1 : x - 1;
-				}
-			}
-		}
-	}
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board[i].size(); j++)
+            {
+                if (board[i][j] <= 0) continue;
+                aliveReport(board, i, j);
+            }
+        }
 
-	for (size_t i = 0; i < board.size(); i++)
-	{
-		for (size_t j = 0; j < board[i].size(); j++)
-		{
-			int &x = board[i][j];
-			if (x > 0)
-			{
-				x = (x == 2 + 1 || x == 3 + 1) ? 1 : 0;
-			}
-			else
-			{
-				x = (x == -3) ? 1 : 0;
-			}
-		}
-	}
-}
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board[i].size(); j++)
+            {
+                int& x = board[i][j];
+                x = (x == 2 + 1 || x == 3 + 1 || x == -3) ? 1 : 0;
+            }
+        }
+    }
+};
+
+
+//////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    int countAlive(vector<vector<int>>& board, int x, int y)
+    {
+        int cnt = 0;
+        for (int i = max(0, x - 1); i < min((int)board.size(), x + 2); i++)
+        {
+            for (int j = max(0, y - 1); j < min((int)board[0].size(), y + 2); j++)
+            {
+                if (i == x && j == y) continue;
+                cnt += (i < x || (i == x && j < y)) ? ((0b10 & board[i][j]) != 0) : ((0b01 & board[i][j]) != 0);
+            }
+        }
+        return cnt;
+    }
+
+    void gameOfLife(vector<vector<int>>& board)
+    {
+        if (board.empty() || board[0].empty()) return;
+
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board[i].size(); j++)
+            {
+                int cnt = countAlive(board, i, j);
+                board[i][j] <<= 1;
+                if ((board[i][j] & 0b10) != 0)
+                {
+                    board[i][j] |= (cnt == 2 || cnt == 3);
+                }
+                else
+                {
+                    board[i][j] |= (cnt == 3);
+                }
+            }
+        }
+
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board[i].size(); j++)
+            {
+                board[i][j] &= 1;
+            }
+        }
+    }
+};
 
 //////////////////////////////////////////////////////////////////////////
 vector<vector<int>> _solution_run(vector<vector<int>>& board)
 {
-	gameOfLife(board);
-	return board;
+    printVectorVectorT(board);
+    Solution2 sln;
+    sln.gameOfLife(board);
+    printVectorVectorT(board);
+    return board;
 }
 
 //#define USE_SOLUTION_CUSTOM
@@ -70,6 +108,6 @@ vector<vector<int>> _solution_run(vector<vector<int>>& board)
 //#define USE_GET_TEST_CASES_IN_CPP
 //vector<string> _get_test_cases_string()
 //{
-//	return {};
+//    return {};
 //}
 
