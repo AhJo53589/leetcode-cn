bd83ed08e4b9f1098d4130122764b480dcb7b6cd
AhJo53589 <ahjo_bb@hotmail.com>
2020-04-09 00:18:18 +0800

    905.sort-array-by-parity

--- problems/generate-parentheses/SOLUTION.cpp
+++ problems/generate-parentheses/SOLUTION.cpp
@@ -19,41 +19,80 @@
 //}
 
 //////////////////////////////////////////////////////////////////////////
-vector<string> generateParenthesis(int n)
-{
-	vector<int> res_left;
-	vector<string> res;
-	res_left.push_back(0);
-	res.push_back("");
+class Solution2 {
+public:
+    vector<string> generateParenthesis(int n)
+    {
+        vector<int> res_left;
+        vector<string> res;
+        res_left.push_back(0);
+        res.push_back("");
 
-	for (int i = 0; i < 2 * n; i++)
-	{
-		int len = res.size();
-		for (int j = 0; j < len; j++)
-		{
-			int left = res_left[j];
-			if (left < n)
-			{
-				res_left.push_back(left + 1);
-				res.push_back(res[j] + "(");
-			}
+        for (int i = 0; i < 2 * n; i++)
+        {
+            int len = res.size();
+            for (int j = 0; j < len; j++)
+            {
+                int left = res_left[j];
+                if (left < n)
+                {
+                    res_left.push_back(left + 1);
+                    res.push_back(res[j] + "(");
+                }
 
-			if (i - left < left)
-			{
-				res_left.push_back(left);
-				res.push_back(res[j] + ")");
-			}
-		}
-		res_left.erase(res_left.begin(), res_left.begin() + len);
-		res.erase(res.begin(), res.begin() + len);
-	}
-	return res;
-}
+                if (i - left < left)
+                {
+                    res_left.push_back(left);
+                    res.push_back(res[j] + ")");
+                }
+            }
+            res_left.erase(res_left.begin(), res_left.begin() + len);
+            res.erase(res.begin(), res.begin() + len);
+        }
+        return res;
+    }
+};
+
+//////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) 
+    {
+        vector<string> ans;
+        queue<pair<string, int>> que;
+        que.push({ "(", 1 });
+
+        while (!que.empty())
+        {
+            auto q = que.front();
+            que.pop();
+
+            if (q.first.size() == 2 * n)
+            {
+                if (q.second != 0) continue;
+                ans.push_back(q.first);
+                continue;
+            }
+
+            if (q.second + 1 <= n)
+            {
+                que.push({ q.first + "(", q.second + 1 });
+            }
+            if (q.second > 0)
+            {
+                que.push({ q.first + ")", q.second - 1 });
+            }
+        }
+
+        return ans;
+    }
+};
 
 //////////////////////////////////////////////////////////////////////////
 vector<string> _solution_run(int n)
 {
-	return generateParenthesis(n);
+	Solution sln;
+	return sln.generateParenthesis(n);
 }
 
 //#define USE_SOLUTION_CUSTOM
