432ddd65ccf75cf029a9703c192273d4a331ae5d
Ikaruga <ahjo_bb@hotmail.com>
2020-04-12 01:53:59 +0800

    905.sort-array-by-parity

--- problems/super-egg-drop/SOLUTION.cpp
+++ problems/super-egg-drop/SOLUTION.cpp
@@ -1,88 +1,85 @@
 
-//int superEggDrop(int K, int N)	// 4ms
+//int superEggDrop(int K, int N)    // 4ms
 //{
-//	int dp[K + 1];
-//	fill(dp, dp + K + 1, 0);
-//	int m = 0;
-//	while (dp[K] < N) {
-//		for (int i = K; i >= 1; i--)
-//			dp[i] = dp[i] + dp[i - 1] + 1;
-//		m++;
-//	}
-//	return m;
+//    int dp[K + 1];
+//    fill(dp, dp + K + 1, 0);
+//    int m = 0;
+//    while (dp[K] < N) {
+//        for (int i = K; i >= 1; i--)
+//            dp[i] = dp[i] + dp[i - 1] + 1;
+//        m++;
+//    }
+//    return m;
 //}
 
 //typedef vector<vector<int>> MI;
 //typedef vector<int> VI;
-//int superEggDrop(int K, int N)	// 8ms
+//int superEggDrop(int K, int N)    // 8ms
 //{
-//	if (K == 0 || N == 0) {
-//		return 0;
-//	}
+//    if (K == 0 || N == 0) {
+//        return 0;
+//    }
 //
-//	if (N == 1) {
-//		return 1;
-//	}
+//    if (N == 1) {
+//        return 1;
+//    }
 //
-//	if (K == 1) {
-//		return N;
-//	}
-//	MI mi;
-//	VI vi;
-//	for (int j = 0; j < K + 1; ++j) {
-//		vi.push_back(0);
-//	}
-//	mi.push_back(vi);
-//	for (int x = 1; x < N + 1; ++x) {
-//		VI vi;
-//		vi.push_back(0);
-//		vi.push_back(x);
+//    if (K == 1) {
+//        return N;
+//    }
+//    MI mi;
+//    VI vi;
+//    for (int j = 0; j < K + 1; ++j) {
+//        vi.push_back(0);
+//    }
+//    mi.push_back(vi);
+//    for (int x = 1; x < N + 1; ++x) {
+//        VI vi;
+//        vi.push_back(0);
+//        vi.push_back(x);
 //
-//		int n = 0;
-//		for (int k = 2; k < K + 1; ++k) {
-//			VI& tmp = mi[x - 1];
-//			n = tmp[k - 1] + tmp[k] + 1;
-//			vi.push_back(n);
-//		}
+//        int n = 0;
+//        for (int k = 2; k < K + 1; ++k) {
+//            VI& tmp = mi[x - 1];
+//            n = tmp[k - 1] + tmp[k] + 1;
+//            vi.push_back(n);
+//        }
 //
-//		if (n >= N) {
-//			return x;
-//		}
+//        if (n >= N) {
+//            return x;
+//        }
 //
-//		mi.push_back(vi);
-//	}
+//        mi.push_back(vi);
+//    }
 //
-//	return N;
+//    return N;
 //}
 
+
 //////////////////////////////////////////////////////////////////////////
-int calcMaximumCoverage(int K, int T)
-{
-	// 有 K 个蛋，行动 T 次，计算能判断多少层
+class Solution {
+public:
+    int calcF(int K, int T)
+    {
+        if (T == 1 || K == 1) return T + 1;
+        return calcF(K - 1, T - 1) + calcF(K, T - 1);
+    }
 
-	//if (T == 1) return 2;		// 如果只有 1 次行动了，无论有几个蛋，都只能判断出 2 层
-	//if (K == 1) return T + 1;	// 如果只有 1 个蛋了，蛋不能碎，只能一层一层判断，可以判断出 T + 1 层
-	if (T == 1 || K == 1) return T + 1;	// 上面两句可以合并成这一句
-
-	// 行动一次，扔个蛋，计算蛋碎了的情况下能判断多少层 + 蛋没碎的情况能判断多少层
-	return calcMaximumCoverage(K - 1, T - 1) + calcMaximumCoverage(K, T - 1);
-}
-
-int superEggDrop(int K, int N)	// 8ms
-{
-	int ans = 1;
-	while (calcMaximumCoverage(K, ans) < N + 1)
-	{
-		++ans;
-	}
-	return ans;
-}
-
+    int superEggDrop(int K, int N)
+    {
+        int T = 1;
+        while (calcF(K, T) < N + 1) T++;
+        cout << calcF(K, T) << endl;
+        return T;
+    }
+};
 
 //////////////////////////////////////////////////////////////////////////
 int _solution_run(int K, int N)
 {
-	return superEggDrop(K,N);
+    cout << "K = " << K << "\tN = " << N << endl;
+    Solution sln;
+    return sln.superEggDrop(K,N);
 }
 
 //#define USE_SOLUTION_CUSTOM
@@ -94,6 +91,6 @@ int _solution_run(int K, int N)
 //#define USE_GET_TEST_CASES_IN_CPP
 //vector<string> _get_test_cases_string()
 //{
-//	return {};
+//    return {};
 //}
 
