07061002ae6ffd86cbb4262aa23c1385c96b94ab
AhJo53589 <ahjo_bb@hotmail.com>
2020-05-07 01:46:34 +0800

    98.validate-binary-search-tree

---
+++ problems_test/983/SOLUTION.cpp
@@ -0,0 +1,32 @@
+
+//////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+
+    }
+};
+
+//////////////////////////////////////////////////////////////////////////
+int _solution_run(vector<int>& days, vector<int>& costs)
+{
+    //int caseNo = -1;
+    //static int caseCnt = 0;
+    //if (caseNo != -1 && caseCnt++ != caseNo) return {};
+
+    Solution sln;
+    return sln.mincostTickets(days, costs);
+}
+
+//#define USE_SOLUTION_CUSTOM
+//string _solution_custom(TestCases &tc)
+//{
+//    return {};
+//}
+
+//////////////////////////////////////////////////////////////////////////
+//#define USE_GET_TEST_CASES_IN_CPP
+//vector<string> _get_test_cases_string()
+//{
+//    return {};
+//}
