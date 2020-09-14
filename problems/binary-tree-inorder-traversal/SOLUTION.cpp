715262818023f5291e61095d4bf09c6260a352f0
zhouzhiyong2020 <ahjo_bb@hotmail.com>
2020-09-14 13:52:50 +0800

    5514.check-if-string-is-transformable-with-substring-sort-operations

--- problems/binary-tree-inorder-traversal/SOLUTION.cpp
+++ problems/binary-tree-inorder-traversal/SOLUTION.cpp
@@ -2,26 +2,62 @@
 
 
 //////////////////////////////////////////////////////////////////////////
-vector<int> inorderTraversal(TreeNode* root)
-{
-	if (root == nullptr) return {};
-	vector<int> ret;
-	vector<int> left;
-	vector<int> right;
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode* pre = nullptr;
+        vector<int> ans;
+        while (root != nullptr) {
+            if (root->left == nullptr) {
+                ans.push_back(root->val);
+                root = root->right;
+            }
+            else {
+                pre = root->left;
+                while (pre->right != nullptr && pre->right != root) {
+                    pre = pre->right;
+                }
+                if (pre->right == nullptr) {
+                    pre->right = root;
+                    root = root->left;
+                }
+                else {
+                    ans.push_back(root->val);
+                    pre->right = nullptr;
+                    root = root->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
 
-	if (root->left != nullptr) left = inorderTraversal(root->left);
-	ret.insert(ret.end(), left.begin(), left.end());
-	ret.push_back(root->val);
-	if (root->right != nullptr) right = inorderTraversal(root->right);
-	ret.insert(ret.end(), right.begin(), right.end());
-	return ret;
-}
+
+//////////////////////////////////////////////////////////////////////////
+class Solution2 {
+public:
+    vector<int> inorderTraversal(TreeNode* root)
+    {
+        if (root == nullptr) return {};
+        vector<int> ret;
+        vector<int> left;
+        vector<int> right;
+
+        if (root->left != nullptr) left = inorderTraversal(root->left);
+        ret.insert(ret.end(), left.begin(), left.end());
+        ret.push_back(root->val);
+        if (root->right != nullptr) right = inorderTraversal(root->right);
+        ret.insert(ret.end(), right.begin(), right.end());
+        return ret;
+    }
+};
 
 
 //////////////////////////////////////////////////////////////////////////
 vector<int> _solution_run(TreeNode* root)
 {
-	return inorderTraversal(root);
+    Solution sln;
+	return sln.inorderTraversal(root);
 }
 
 //#define USE_SOLUTION_CUSTOM
