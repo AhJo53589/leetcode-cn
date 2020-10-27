b7345539ec33d559e189a787946bda7687264524
zhouzhiyong2020 <ahjo_bb@hotmail.com>
2020-10-27 22:07:30 +0800

    5156.rank-transform-of-a-matrix

--- problems/rank-transform-of-a-matrix/SOLUTION.cpp
+++ problems/rank-transform-of-a-matrix/SOLUTION.cpp
@@ -3,7 +3,10 @@
 class Solution {
 public:
     vector<vector<int>> matrixRankTransform(vector<vector<int>>& matrix) {
-        vector<vector<int>> ans(matrix.size(), vector<int>(matrix[0].size(), 0));
+        ans = vector<vector<int>>(matrix.size(), vector<int>(matrix[0].size(), -1));
+        row = vector<bool>(matrix.size(), false);
+        col = vector<bool>(matrix[0].size(), false);
+
         vector<vector<int>> seq(matrix.size() * matrix[0].size());
         int k = 0;
         for (int i = 0; i < matrix.size(); i++) {
@@ -14,48 +17,60 @@ public:
         sort(seq.begin(), seq.end());
 
         for (int i = 0; i < seq.size(); i++) {
-            calc(matrix, ans, seq[i][1], seq[i][2]);
+            int x = seq[i][1];
+            int y = seq[i][2];
+            if (ans[x][y] != -1) continue;
+            vi_clear(matrix);
+            vi_fill(matrix, x, y);
+            int t = calcRank(matrix, matrix[x][y]);
+            setRank(matrix, t);
         }
         return ans;
     }
 
-    void calc(vector<vector<int>>& matrix, vector<vector<int>>& ans, int x, int y) {
-        int t = 1;
-        vector<vector<int>> same;
+    void vi_fill(vector<vector<int>>& matrix, int x, int y) {
+        ans[x][y] = 0;
+        row[x] = true;
+        col[y] = true;
         for (int i = 0; i < matrix.size(); i++) {
-            if (matrix[i][y] > matrix[x][y]) continue;
-            if (matrix[i][y] == matrix[x][y]) {
-                same.push_back({ i, y });
-                t = max(t, ans[i][y]);
-            }
-            else {
-                t = max(t, ans[i][y] + 1);
-            }
+            if (matrix[i][y] != matrix[x][y] || ans[i][y] != -1 || row[i]) continue;
+            vi_fill(matrix, i, y);
         }
         for (int j = 0; j < matrix[0].size(); j++) {
-            if (matrix[x][j] > matrix[x][y]) continue;
-            if (matrix[x][j] == matrix[x][y]) {
-                same.push_back({ x, j });
-                t = max(t, ans[x][j]);
-            }
-            else {
-                t = max(t, ans[x][j] + 1);
-            }
-        }
-        set<vector<int>> vi;
-        queue<vector<int>> que;
-        for (auto s : same) {
-            que.push({ s[0], s[1] });
-            vi.insert({ s[0], s[1] });
-            //ans[s[0]][s[1]] = t;
-        }
-        while (!que.empty()) {
-            auto q = que.front();
-            que.pop();
-
-
+            if (matrix[x][j] != matrix[x][y] || ans[x][j] != -1 || col[j]) continue;
+            vi_fill(matrix, x, j);
         }
     }
+
+    void vi_clear(vector<vector<int>>& matrix) {
+        row = vector<bool>(matrix.size(), false);
+        col = vector<bool>(matrix[0].size(), false);
+    }
+
+    int calcRank(vector<vector<int>>& matrix, int val) {
+        int t = 1;
+        for (int i = 0; i < row.size(); i++) {
+            if (!row[i]) continue;
+            for (int j = 0; j < col.size(); j++) {
+                if (!col[j]) continue;
+                t = (matrix[i][j] < val) ? max(t, ans[i][j] + 1) : t;
+            }
+        }
+        return t;
+    }
+
+    void setRank(vector<vector<int>>& matrix, int rank) {
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                ans[i][j] = (ans[i][j] == 0) ? rank : ans[i][j];
+            }
+        }
+    }
+
+private:
+    vector<vector<int>> ans;
+    vector<bool> row;
+    vector<bool> col;
 };
 
 //////////////////////////////////////////////////////////////////////////
