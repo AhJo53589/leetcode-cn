8aa248761ce7283a2f384765a4db029cd8f660aa
zhouzhiyong2020 <ahjo_bb@hotmail.com>
2021-11-08 14:47:25 +0800

    5921.maximum-path-quality-of-a-graph

--- problems/maximum-path-quality-of-a-graph/README.md
+++ problems/maximum-path-quality-of-a-graph/README.md
@@ -86,8 +86,41 @@
 class Solution {
 public:
     int maximalPathQuality(vector<int>& values, vector<vector<int>>& edges, int maxTime) {
-        
+        this->values = values;
+
+        for (auto& edge : edges) {
+            paths[edge[0]].push_back({ edge[1], edge[2] });
+            paths[edge[1]].push_back({ edge[0], edge[2] });
+        }
+
+        dfs(0, 0, 0, maxTime);
+
+        return ans;
     }
+
+    void dfs(int nd, int curValue, int curTime, int maxTime) {
+        int v = values[nd];
+        values[nd] = 0;
+        curValue += v;
+
+        if (nd == 0) {
+            ans = max(ans, curValue);
+        }
+
+        for (auto& p : paths[nd]) {
+            int nt = curTime + p.second;
+            if (nt > maxTime) continue;
+            dfs(p.first, curValue, nt, maxTime);
+        }
+
+        curValue -= v;
+        values[nd] = v;
+    }
+
+private:
+    vector<int> values;
+    unordered_map<int, vector<pair<int, int>>> paths;
+    int ans;
 };
 ```
 
--- problems/maximum-path-quality-of-a-graph/SOLUTION.cpp
+++ problems/maximum-path-quality-of-a-graph/SOLUTION.cpp
@@ -3,44 +3,41 @@
 class Solution {
 public:
     int maximalPathQuality(vector<int>& values, vector<vector<int>>& edges, int maxTime) {
-        vector<vector<int>> path(values.size(), vector<int>(values.size(), 0));
+        this->values = values;
+
         for (auto& edge : edges) {
-            path[edge[0]][edge[1]] = edge[2];
-            path[edge[1]][edge[0]] = edge[2];
+            paths[edge[0]].push_back({ edge[1], edge[2] });
+            paths[edge[1]].push_back({ edge[0], edge[2] });
         }
 
-        int ans = values[0];
+        dfs(0, 0, 0, maxTime);
 
-        queue<tuple<int, int, bitset<1001>>> pq;
-        bitset<1001> b(1);
-        pq.push({ 0, 0, b });
-        while (!pq.empty()) {
-            auto [t, nd, val] = pq.front();
-            pq.pop();
-
-            for (int i = 0; i < path[nd].size(); i++) {
-                if (path[nd][i] == 0) continue;
-                auto cv = val[i];
-                cv.set(i, 1);
-                int ct = t + path[nd][i];
-                if (ct > maxTime) continue;
-
-                pq.push({ ct, i, cv });
-                if (i == 0) {
-                    ans = max(ans, getVal(values, cv));
-                }
-            }
-        }
         return ans;
     }
 
-    int getVal(vector<int>& values, bitset<1000> cv) {
-        int val = 0;
-        for (int i = 0; i < values.size(); i++) {
-            val += cv[i] * values[i];
+    void dfs(int nd, int curValue, int curTime, int maxTime) {
+        int v = values[nd];
+        values[nd] = 0;
+        curValue += v;
+
+        if (nd == 0) {
+            ans = max(ans, curValue);
         }
-        return val;
+
+        for (auto& p : paths[nd]) {
+            int nt = curTime + p.second;
+            if (nt > maxTime) continue;
+            dfs(p.first, curValue, nt, maxTime);
+        }
+
+        curValue -= v;
+        values[nd] = v;
     }
+
+private:
+    vector<int> values;
+    unordered_map<int, vector<pair<int, int>>> paths;
+    int ans;
 };
 
 //////////////////////////////////////////////////////////////////////////
